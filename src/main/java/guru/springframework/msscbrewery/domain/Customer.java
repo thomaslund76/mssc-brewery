385ede4797ffd67d488bcf9012ab28b1704cab4e
Thomas Lund <thomaslund76@gmail.com>
2020-01-21 11:57:17 +0100

    Added mapstruct for customer - customerDto conversion

---
+++ src/main/java/guru/springframework/msscbrewery/domain/Customer.java
@@ -0,0 +1,19 @@
+package guru.springframework.msscbrewery.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+    private UUID id;
+    private String name;
+}
